aa703cd3ff4d684a85b083cdae78bebfdecf741f
Morgan Speake <gq24940@bristol.ac.uk>
2026-03-05 17:05:24 +0000

    Commit unifnished work

--- main.py
+++ main.py
@@ -1,35 +1,70 @@
-import numpy as np
-import matplotlib.pyplot as plt
+#!/bin/env python
 
-def mock_velocity_field(x: float, z: float, t: float, nu: float) -> tuple[float, float]:
+import numpy as np
+import ot
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+
+def random_positions(count: int, scale: float) -> np.ndarray:
+    return (np.random.random((count, 2)) - 0.5) * scale
+
+
+def mock_velocity_field(position, t, nu) -> np.ndarray:
     # Eq. 1 and 2 from project brief.
     e = np.exp(-2.0 * nu * t)
-    u = np.sin(x) * np.cos(z) * e
-    w = -np.cos(x) * np.sin(z) * e
-    return (u, w)
+
+    velocity = np.zeros(position.shape)
+
+    velocity[:, 0] = np.sin(position[:, 0]) * np.cos(position[:, 1]) * e
+    velocity[:, 1] = -np.cos(position[:, 0]) * np.sin(position[:, 1]) * e
+    return velocity
 
 
-def initial_positions(count: int, scale: float) -> tuple[np.ndarray, np.ndarray]:
-    xs = (np.random.random(count) - 0.5) * scale
-    zs = (np.random.random(count) - 0.5) * scale
-    return (xs, zs)
+def integrate_position(positions, t, nu):
+    velocity = mock_velocity_field(positions, t, nu)
+    positions += velocity * t
 
+print("posistions...")
 
-def integrate_position(x: float, z: float, t: float, nu: float) -> tuple[float, float]:
-    (u, w) = mock_velocity_field(x, z, t, nu)
-    x += u * t
-    z += w * t
-    return (x, z)
+count = 500
+first = random_positions(count, 1)
 
-timescale = 0.01
-nu = 1
-xs, zs = initial_positions(10000, 10.0)
-for i in range(1000):
-    print(f"[{i:4}/1000] generating...")
-    fig, ax = plt.subplots()
-    ax.scatter(xs, zs, s=10)
-    fig.savefig(f"frames/{i}.png")
-    plt.close(fig)
-    (xs, zs) = integrate_position(xs, zs, i * timescale, nu)
+second = np.copy(first)
+integrate_position(second, 0.2, 1.0)
 
-print("[1000/1000] done")
+np.random.shuffle(first)
+np.random.shuffle(second)
+
+print("cost matrix...")
+
+costs = np.zeros((count, count))
+for i in range(count):
+    for j in range(count):
+        dx = first[i, 0] - second[j, 0]
+        dy = first[i, 1] - second[j, 1]
+        distance = np.abs((dx * dx) + (dy * dy))
+        costs[i, j] = distance
+
+print("assignments...")
+
+plan = ot.solve(costs).plan
+
+print("figure...")
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.set_title("Particle assignment between frames")
+
+ax.scatter(first[:, 0], first[:, 1], c="r", label="Previous")
+ax.scatter(second[:, 0], second[:, 1], c="b", label="Next")
+
+for i in range(count):
+    for j in range(count):
+        if plan[i, j] != 0:
+            xs = [first[i, 0], second[j, 0]]
+            ys = [first[i, 1], second[j, 1]]
+            ax.add_line(Line2D(xs, ys, color="grey"))
+
+ax.legend()
+
+fig.savefig("figure.png", dpi=100)
